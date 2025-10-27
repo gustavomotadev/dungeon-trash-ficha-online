aeae1e46233315f949c3eda319801a13b748c2df
gustavomotadev <gustavomotadev@gmail.com>
2025-10-27 20:54:05 -0300

    Character gen numbers ok without classes

--- script.js
+++ script.js
@@ -13,40 +13,68 @@ function d(lados) {
     return Math.floor(Math.random() * (lados + 1));
 }
 
-function rolarTabelas() {
+function sortearTabela(tabela) {
+    return tabela[Math.floor(Math.random() * tabela.length)];
+}
 
+function rolarTabelas(tabelas) {
+
+    console.debug(tabelas);
+
+    const roupas = sortearTabela(tabelas["roupas"]);
+    const defesas = sortearTabela(tabelas["defesas"]);
+    const armas = sortearTabela(tabelas["armas"]);
+
+    const resultado = { roupas: roupas, defesas: defesas, armas: armas }
+
+    console.log(resultado);
+
+    return resultado;
 }
 
 function gerarPersonagem(tabelas) {
 
-    console.debug(tabelas);
-
-    let classe = document.getElementById('classe').value;
+    const classe = document.getElementById('classe').value;
     console.debug(classe);
 
-    let rolagem_agilidade = d(6) + d(6) + d(6);
-    let agilidade = modificador(rolagem_agilidade);
+    const rolagem_agilidade = d(6) + d(6) + d(6);
+    const agilidade = modificador(rolagem_agilidade);
     console.log(rolagem_agilidade, agilidade);
 
-    let rolagem_fisico = d(6) + d(6) + d(6);
-    let fisico = modificador(rolagem_fisico);
+    const rolagem_fisico = d(6) + d(6) + d(6);
+    const fisico = modificador(rolagem_fisico);
     console.log(rolagem_fisico, fisico);
 
-    let rolagem_mental = d(6) + d(6) + d(6);
-    let mental = modificador(rolagem_mental);
+    const rolagem_mental = d(6) + d(6) + d(6);
+    const mental = modificador(rolagem_mental);
     console.log(rolagem_mental, mental);
 
-    let rolagem_vida = d(6) + d(6);
-    let vida = rolagem_vida + agilidade + fisico + mental;
+    const rolagem_vida = d(6) + d(6);
+    const vida = rolagem_vida + agilidade + fisico + mental;
     console.log(rolagem_vida, vida);
 
-    let carga_padrao = 10 + fisico;
-    let carga_maxima = carga_padrao * 2;
+    const carga_padrao = 10 + fisico;
+    const carga_maxima = carga_padrao * 2;
     console.log(carga_padrao, carga_maxima);
 
-    let comida_agua = d(6);
-    let agua_suja = d(2) > 0;
+    const comida_agua = d(6);
+    const agua_suja = d(2) > 0;
     console.debug(comida_agua, agua_suja);
+
+    const { roupas, defesas, armas } = rolarTabelas(tabelas);
+
+    const personagem = {
+        agilidade: agilidade, fisico: fisico,
+        mental: mental, rolagem_vida: rolagem_vida,
+        vida: vida, carga_padrao: carga_padrao,
+        carga_maxima: carga_maxima, comida_agua: comida_agua,
+        agua_suja: agua_suja, roupas: roupas,
+        defesas: defesas, armas: armas
+    };
+
+    console.debug(personagem);
+
+    return personagem;
 }
 
 async function carregarJSON(caminho) {
@@ -61,12 +89,37 @@ async function carregarJSON(caminho) {
     }
 }
 
+function verificarTabelas(tabelas) {
+
+    if (!(('roupas' in tabelas) &&
+        ('defesas' in tabelas) &&
+        ('armas' in tabelas))) return false;
+
+    return true;
+}
+
+function habilitarGeracao(tabelas) {
+
+    const botaoGerar = document.getElementById('gerar');
+
+    botaoGerar.addEventListener('click', () => gerarPersonagem(tabelas));
+
+    botaoGerar.disabled = false;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // Code here runs once, after the DOM is loaded
     console.debug('TRASH!');
 
     carregarJSON('tabelas.json').then(resultado => {
-        if (resultado.sucesso) gerarPersonagem(resultado.dados);
+        if (resultado.sucesso && verificarTabelas(resultado.dados)) {
+
+            habilitarGeracao(resultado.dados);
+        
+        } else {
+
+            throw new Error('Problema nas tabelas');
+        }
     });
 
 });
